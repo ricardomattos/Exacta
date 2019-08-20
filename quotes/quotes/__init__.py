c24df732b5f84f0f61d4794fa3c736bc483c7dd1
ricardo mattos <ricardo.svmtts@gmail.com>
2019-08-20 00:07:22 +0000

    removing blank lines

--- quotes/quotes/__init__.py
+++ quotes/quotes/__init__.py
@@ -2,14 +2,11 @@ import json
 import random
 import requests
 
-
 DEFAULT_URL = 'https://1c22eh3aj8.execute-api.us-east-1.amazonaws.com/challenge/{}/'
-
 
 def unserialize(content):
     """Unserialize objects"""
     return json.loads(content)
-
 
 def make_request(url):
     """Make request to api"""
@@ -17,13 +14,11 @@ def make_request(url):
     content = resp.content.decode('utf-8')
     return unserialize(content)
 
-
 def get_quotes():
     """Get all quotes from api"""
     url = DEFAULT_URL.format('quotes')
     resp = make_request(url)
     return resp['quotes']
-
 
 def get_quote(quote_number):
     """Get one quote from api"""
