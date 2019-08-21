a040b9ba178cdc908ceaf57609050cb45caac377
ricardo mattos <ricardo.svmtts@gmail.com>
2019-08-21 20:14:02 +0000

    adjust quotes tests

--- quotes/tests/test_quotes.py
+++ quotes/tests/test_quotes.py
@@ -1,35 +1,25 @@
-import unittest
 import requests
 import quotes
 
 
-class APITest(unittest.TestCase):
-    """API test"""
+def test_status_api():
+    """test api returns with http 200"""
+    response = requests.get(quotes.DEFAULT_URL.format('quotes'))
+    assert response.status_code == 200
 
-    def test_status_api(self):
-        """test api returns with http 200"""
-        response = requests.get(quotes.DEFAULT_URL.format('quotes'))
-        self.assertEqual(response.status_code, 200)
+def test_return_correct_quote():
+    """test return the correct quote"""
+    obj = quotes.get_quote(1)
+    print(obj)
+    assert 'Explicit is better than implicit.' == obj
 
+def test_return_quotes():
+    """test return an obj with more than one quote"""
+    obj = quotes.get_quotes()
+    assert len(obj) > 0
 
-class GetQuoteTest(unittest.TestCase):
-    """get_quote tests"""
-
-    def test_return_correct_quote(self):
-        """test return the correct quote"""
-        obj = quotes.get_quote(1)
-        self.assertIn('Explicit is better than implicit', obj)
-
-
-class GetQuotesTest(unittest.TestCase):
-    """get_quotes tests"""
-
-    def test_return_quotes(self):
-        """test return an obj with more than one quote"""
-        obj = quotes.get_quotes()
-        self.assertTrue(len(obj) > 0)
-
-    def test_return_correct_quote(self):
-        """test return the correct quote"""
-        obj = quotes.get_quotes()
-        self.assertIn('Explicit is better than implicit.', obj)
+def test_return_correct_quotes():
+    """test return a quote"""
+    obj = quotes.get_quotes()
+    print(obj)
+    assert 'Simple is better than complex.' in obj
